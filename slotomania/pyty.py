80258394150d12cfa0b6f80343fcb2865fa9b15c
Conan Li <conanlics@gmail.com>
2019-02-09 20:24:31 -0500

    wip

--- slotomania/pyty.py
+++ slotomania/pyty.py
@@ -21,6 +21,7 @@ Thing {
 }
 
 """
+import re
 from typing import Type
 
 
@@ -28,9 +29,12 @@ class FieldType:
     def __init__(self, *__type_args__) -> None:
         self.__type_args__ = list(__type_args__)
 
+    def to_dict(self) -> dict:
+        raise NotImplementedError()
+
 
 class Primitive(FieldType):
-    def to_dict(self):
+    def to_dict(self) -> dict:
         return {"__type__": self.__class__.__name__}
 
 
@@ -56,19 +60,25 @@ class Decimal(Primitive):
 
 
 class Array(Composite):
-    def __init__(self, element_type: str) -> None:
-        super().__init__(element_type)
+    def __init__(self, element_type_string: str) -> None:
+        super().__init__(element_type_string)
+        self.element_type_string = element_type_string
 
-    def to_dict(self):
-        return {
-            "__type__": self.__class__.__name__,
-            "__type_args__": self.__type_args__,
-        }
+    def to_dict(self) -> dict:
+        return {"__type__": f"{self.__class__.__name__}<{self.element_type_string}>"}
 
 
-def get_primitive_type(type_name: str) -> Type[FieldType]:
+def get_type_class_by_name(type_name: str) -> Type[FieldType]:
     lookup = {k.__name__: k for k in [String, Integer, Boolean, Decimal, Array]}
     return lookup[type_name]
+
+
+def parse_type(field_type_string: str) -> FieldType:
+    match = re.match(r"(\w+)(<.+>)?", field_type_string)
+    if not match:
+        raise Exception(f"Invalid type: {field_type_string}")
+    type_name, type_args = match.groups()
+    return get_type_class_by_name(type_name)(type_args[1:-1] if type_args else [])
 
 
 class Shape:
@@ -83,13 +93,14 @@ class Shape:
         for field_name, field in fields_dict.items():
             # Either a primitive or a defined shape
             field_type_string = field["__type__"]
-            type_args = field.get("__type_args__", [])
-            assert isinstance(type_args, list)
-            field_type = (
-                get_primitive_type(field_type_string)
-                or defined_shapes[field_type_string]
-            )
-            fields[field_name] = field_type(*type_args)
+            fields[field_name] = parse_type(field_type_string)
+            # type_args = field.get("__type_args__", [])
+            # assert isinstance(type_args, list)
+            # field_type = (
+            #     get_type_class_by_name(field_type_string)
+            #     or defined_shapes[field_type_string]
+            # )
+            # fields[field_name] = field_type(*type_args)
 
         return Shape(identifier=identifier, fields=fields)
 
